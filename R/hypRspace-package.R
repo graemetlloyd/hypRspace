2204fab353b1dcfbb57ac5ed9b4269729dc873cb
Graeme T. Lloyd <graemetlloyd@gmail.com>
2018-10-01 14:08:33 +0100

    Early devtools check() starting files.

---
+++ R/hypRspace-package.R
@@ -0,0 +1,25 @@
+# devtools::build_win(version = "R-devel")
+
+#' Biological Hyperspace Functions
+#' 
+#' Functions for biological hyperspaces.
+#' 
+#' @name hypRspace-package
+#'
+#' @aliases hypRspace
+#'
+#' @docType package
+#'
+#' @author Graeme T. Lloyd <graemetlloyd@@gmail.com>
+#'
+#' @references Nothing yet.
+#'
+#' @keywords biology,hyperspace
+#'
+#' @examples
+#' 
+#' # Nothing yet.
+#'
+#' @import ape
+#' @import Claddis
+NULL
